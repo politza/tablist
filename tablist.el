c762a78f44b789d81c86609dadbe71ef71a64743
Andreas Politz <politza@hochschule-trier.de>
2019-01-29 19:10:09 +0100

    Bump version to 0.80

--- tablist.el
+++ tablist.el
@@ -5,7 +5,7 @@
 ;; Author: Andreas Politz <politza@fh-trier.de>
 ;; Keywords: extensions, lisp
 ;; Package: tablist
-;; Version: 0.70
+;; Version: 0.80
 ;; Package-Requires: ((emacs "24.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
